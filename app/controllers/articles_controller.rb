72b5646fee74130e3125a03a74ae29aa537b0e93
camron <camronmoradi@gmail.com>
2018-11-25 00:37:55 +0100

    Adds Flash message to confirm article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     @article.save
+    flash[:notice] = "Article was successfully created."
     redirect_to @article
   end
 
